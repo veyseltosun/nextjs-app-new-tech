e7a869bd44536e30e56b3e6cbcc3b2fe6d2bdd12
veyseltosun <postalarim1923@gmail.com>
2023-06-14 13:30:32 +0300

    some more changes.

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -18,10 +18,24 @@ export default function Home() {
           </nav>
         </header>
         <div>
+          <a href="">
           <img/>
           
           <h2>Events in London</h2>
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </a>
+
+          <a href="">
+          <img/>
+          <h2>Events in Barcelona</h2>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </a>
+          
+          <a href="">
+          <img/>
+          <h2>Events in Paris</h2>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </a>
         </div>
         
         
--- my-app/pages/events/event.js
+++ my-app/pages/events/event.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>event</div>
+    <div>
+      <h1>Events Page</h1>
+      <div>
+      <a href="">
+          <img/>
+          
+          <h2>Events in London</h2>
+          
+          </a>
+
+          <a href="">
+          <img/>
+          <h2>Events in Barcelona</h2>
+          
+          </a>
+          
+          <a href="">
+          <img/>
+          <h2>Events in Paris</h2>
+          
+          </a>
+      </div>
+    </div>
   )
 }
 
